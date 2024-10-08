8f5e85c92360205c6f22fea8b9defb42c9c8afdf
Your GitHub Username <Your GitHub Email>
2024-10-08 03:12:42 -0600

    Added changes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,23 +7,23 @@ import { Typewriter } from "react-simple-typewriter";
 // import React, { useState } from "react";
 
 export default function Hero() {
-  
-    // Define the colors you want to cycle through
-    const colors = ['#134f44', '#F1875E', '#3A3A3A', 'white'];
-    
-    // State to keep track of the current color index
-    const [currentColorIndex, setCurrentColorIndex] = useState(0);
-    
-    // Update color every 2 seconds
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setCurrentColorIndex((prevIndex:number) => (prevIndex + 1) % colors.length);
-      }, 2000);
-  
-      // Cleanup interval on unmount
-      return () => clearInterval(intervalId);
-    }, []);
-  
+  // Define the colors you want to cycle through
+  const colors = ["#134f44", "#F1875E", "#3A3A3A", "white"];
+
+  // State to keep track of the current color index
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+
+  // Update color every 2 seconds
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentColorIndex(
+        (prevIndex: number) => (prevIndex + 1) % colors.length
+      );
+    }, 2000);
+
+    // Cleanup interval on unmount
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div>
@@ -31,17 +31,19 @@ export default function Hero() {
         className="bg-image bg-cover bg-center bg-no-repeat flex flex-col px-4 py-6 pt-10 sm:pt-20 relative"
         style={{
           backgroundImage: 'url("/images/bg.svg")',
-          minHeight: "70vh",  // Ensures the height is responsive
+          minHeight: "70vh", // Ensures the height is responsive
           height: "auto",
         }}
       >
         {/* Main Heading */}
-        <div className="container mx-auto relative z-10 text-center mt-auto"> {/* Added mt-auto */}
+        <div className="container mx-auto relative z-10 text-center mt-auto">
+          {" "}
+          {/* Added mt-auto */}
           <h1
-           className="text-transparent text-6xl sm:text-7xl md:text-9xl font-bold pb-6"
-           style={{
-            WebkitTextStroke: `1px ${colors[currentColorIndex]}`,
-          }}
+            className="text-transparent text-6xl sm:text-7xl md:text-9xl font-bold pb-6"
+            style={{
+              WebkitTextStroke: `1px ${colors[currentColorIndex]}`,
+            }}
           >
             Fullstack Web Developer
           </h1>
@@ -57,75 +59,86 @@ export default function Hero() {
         </div>
 
         {/* Content Section */}
-        <div className="flex flex-col sm:flex-row sm:justify-between w-full relative z-10 items-center space-y-5 sm:space-y-0 sm:px-6 mt-auto"> {/* Added mt-auto */}
+        <div className="flex flex-col sm:flex-row sm:justify-between w-full relative z-10 items-center space-y-5 sm:space-y-0 sm:px-6 mt-auto">
+          {" "}
+          {/* Added mt-auto */}
           {/* Left Column */}
           <div className="flex flex-col space-y-3 sm:space-y-5 sm:w-1/2 w-full justify-center items-center sm:items-start text-center sm:text-left">
             {/* Text */}
             <div className="text-2xl sm:text-4xl md:text-6xl font-bold">
-            <Fade direction="right" triggerOnce><p className="hidden sm:block  text-white">
-               Hey There,<br /> I’m Khadi
-              </p></Fade> 
-              <Fade direction="up" triggerOnce >  <p className="sm:hidden  text-white">Hey There, I’m Khadi</p> </Fade> {/* Mobile view */}
+              <Fade direction="right" triggerOnce>
+                <p className="hidden sm:block  text-white">
+                  Hey There,
+                  <br /> I’m Khadi
+                </p>
+              </Fade>
+              <Fade direction="up" triggerOnce>
+                {" "}
+                <p className="sm:hidden  text-white">
+                  Hey There, I’m Khadi
+                </p>{" "}
+              </Fade>{" "}
+              {/* Mobile view */}
             </div>
 
             {/* Button */}
-            <Fade direction="up" triggerOnce >
-            <div className="flex justify-center sm:justify-start w-full">
-              <button className="text-white font-bold bg-[#F1875E] px-4 py-2 sm:px-6 sm:py-3 rounded-lg shadow-md hover:bg-[#f07850] transition-all duration-300 text-sm sm:text-base">
-                Fullstack dev
-              </button>
-            </div></Fade>
+            <Fade direction="up" triggerOnce>
+              <div className="flex justify-center sm:justify-start w-full">
+                <button className="text-white font-bold bg-[#F1875E] px-4 py-2 sm:px-6 sm:py-3 rounded-lg shadow-md hover:bg-[#f07850] transition-all duration-300 text-sm sm:text-base">
+                  Fullstack dev
+                </button>
+              </div>
+            </Fade>
           </div>
-
           {/* Right Column */}
           <div className="flex sm:w-1/2 justify-center sm:justify-end w-full">
-      <div className="w-full sm:max-w-md text-sm sm:text-xs">
-        {/* For small screens */}
-        <p className="text-center sm:text-left sm:hidden">
-          <Typewriter
-            words={[
-              "A passionate web developer with expertise in creating dynamic and responsive web applications. With  years of experience in the industry, I have honed my skills in various technologies including HTML, CSS, JavaScript, React.js, and Next.js."
-            ]}
-            loop={1}
-            cursor
-            cursorStyle="|"
-            typeSpeed={50}
-            deleteSpeed={50}
-            delaySpeed={1000}
-          />
-        </p>
+            <div className="w-full sm:max-w-md text-sm sm:text-xs">
+              {/* For small screens */}
+              <p className="text-center sm:text-left sm:hidden  text-white">
+                <Typewriter
+                  words={[
+                    "A passionate web developer with expertise in creating dynamic and responsive web applications. With  years of experience in the industry, I have honed my skills in various technologies including HTML, CSS, JavaScript, React.js, and Next.js.",
+                  ]}
+                  loop={1}
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={50}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
+              </p>
 
-        {/* For medium screens */}
-        <p className="hidden sm:block md:hidden  text-white">
-          <Typewriter
-            words={[
-              "A passionate web developer with expertise in creating dynamic and responsive web applications. With  years of experience in the industry, I have honed my skills in various technologies including HTML, CSS, JavaScript, React.js, and Next.js."
-            ]}
-            loop={1}
-            cursor
-            cursorStyle="|"
-            typeSpeed={50}
-            deleteSpeed={50}
-            delaySpeed={1000}
-          />
-        </p>
+              {/* For medium screens */}
+              <p className="hidden sm:block md:hidden  text-white">
+                <Typewriter
+                  words={[
+                    "A passionate web developer with expertise in creating dynamic and responsive web applications. With  years of experience in the industry, I have honed my skills in various technologies including HTML, CSS, JavaScript, React.js, and Next.js.",
+                  ]}
+                  loop={1}
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={50}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
+              </p>
 
-        {/* For large screens */}
-        <p className="hidden md:flex justify-end  text-white">
-          <Typewriter
-            words={[
-              "A passionate web developer with expertise in creating dynamic and responsive web applications. With years of experience in the industry, I have honed my skills in various technologies including HTML, CSS, JavaScript, React.js, and Next.js."
-            ]}
-            loop={1}
-            cursor
-            cursorStyle="|"
-            typeSpeed={50}
-            deleteSpeed={50}
-            delaySpeed={1000}
-          />
-        </p>
-      </div>
-    </div>
+              {/* For large screens */}
+              <p className="hidden md:flex justify-end  text-white">
+                <Typewriter
+                  words={[
+                    "A passionate web developer with expertise in creating dynamic and responsive web applications. With years of experience in the industry, I have honed my skills in various technologies including HTML, CSS, JavaScript, React.js, and Next.js.",
+                  ]}
+                  loop={1}
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={50}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
